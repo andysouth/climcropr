b2d847db56c7a8c6a6d7db958c8895b35bb7a6fb
Adam H. Sparks <adamhsparks@gmail.com>
2017-06-26 13:38:27 +1000

    Expand test

--- tests/testthat/test-get_mirca.R
+++ tests/testthat/test-get_mirca.R
@@ -44,3 +44,8 @@ test_that("test that get_mirca creates a cache directory if none exists", {
   # cleanup
   unlink(rappdirs::user_cache_dir("climcropr"), recursive = TRUE)
 })
+
+test_that("test that set_cache uses tempdir() if caching == FALSE", {
+  get_mirca("potatoes", rainfed = TRUE, cache = FALSE)
+  expect_true(!file.exists(file.path(rappdirs::user_cache_dir("climcropr"))))
+})
